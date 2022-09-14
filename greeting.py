1f568abd1c2a157e0b8c55cd52a917437eefb6af
Volodia Nudyk <92189782+vsthereckless@users.noreply.github.com>
2022-09-14 17:32:50 +0300

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,8 +1,21 @@
+from time import sleep
+
+delay = 4
+
 print("Hi, let me introduce myself! My name is Volodia. I'm 25 and I live in Lviv, Ukraine\n\
 Now tell me something about you: ")
+sleep(2)
 
 userName = input("What is your name? ")
-userAge = int(input("How old are you? "))
+
+while True:
+    try: 
+        userAge = int(input("How old are you? "))
+        break
+    except ValueError:
+        print("You age should be a number, you silly :)")
+
+
 userAddress = input("Where do you live? ")
 
 print(f"Hello {userName}! Nice to meet you!")
