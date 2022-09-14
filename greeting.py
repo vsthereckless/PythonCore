ead1ff4be62200916c80170c136201356f2b6a4c
Volodia Nudyk <92189782+vsthereckless@users.noreply.github.com>
2022-09-14 17:18:26 +0300

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,10 @@
+print("Hi, let me introduce myself! My name is Volodia. I'm 25 and I live in Lviv, Ukraine\n\
+Now tell me something about you: ")
+
+userName = input("What is your name? ")
+userAge = int(input("How old are you? "))
+userAddress = input("Where do you live? ")
+
+print(f"Hello {userName}! Nice to meet you!")
+print(f"Your age is {userAge}.")
+print(f"You live in {userAddress}.")
